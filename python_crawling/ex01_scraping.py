9bde9a904f3fc95db9ff212e333001412589be6e
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-04 14:36:34 +0900

    #4. Web Crawling Using asyncio

--- python_crawling/ex01_scraping.py
+++ python_crawling/ex01_scraping.py
@@ -2,36 +2,30 @@
 
 
 from bs4 import BeautifulSoup
+import aiohttp
+import asyncio
 
-html_doc = """
-<html><head><title>The Dormouse's story</title></head>
-<body>
-<p class="title"><b>The Dormouse's story</b></p>
 
-<p class="story">Once upon a time there were three little sisters; and their names were
-<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
-<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-and they lived at the bottom of a well.</p>
+async def fetch(session, url):
+    async with session.get(url) as response:
+        html = await response.text()
+        soup = BeautifulSoup(html, "html.parser")
+        cont_thumb = soup.find_all("div", "cont_thumb")
 
-<p class="story">...</p>
-"""
+        for cont in cont_thumb:
+            title = cont.find("p", "txt_thumb")
 
-soup = BeautifulSoup(html_doc, "html.parser")
-print(soup.prettify())
+            if title:
+                print(title.text)
 
-print()
 
-print(soup.title)
+async def main():
+    BASE_URL = "https://bjpublic.tistory.com/category/%EC%A0%84%EC%B2%B4%20%EC%B6%9C%EA%B0%84%20%EB%8F%84%EC%84%9C"
+    urls = [f"{BASE_URL}?page={i}" for i in range(1, 10)]
 
-print()
+    async with aiohttp.ClientSession() as session:
+        await asyncio.gather(*[fetch(session, url) for url in urls])
 
-print(soup.p)  # 가장 처음에 있는 p 태그를 가져온다.
 
-print()
-
-print(soup.find("p", "story"))  # 첫 번째 인자는 태그, 두 번째 인자는 클래스를 입력하면 된다.
-
-print()
-
-print(soup.find("p", "story").text)
+if __name__ == "__main__":
+    asyncio.run(main())
