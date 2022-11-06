9b530d208798d659de37609fb070a4c85fdbde99
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-06 21:49:03 +0900

    #6. FastAPI + MongoDB ODM Set Up

--- app/main.py
+++ app/main.py
@@ -6,6 +6,11 @@ from fastapi.templating import Jinja2Templates
 
 from pathlib import Path
 
+from app.models import mongodb
+
+from app.models.book import BookModel
+
+
 BASE_DIR = Path(__file__).resolve().parent
 
 app = FastAPI()
@@ -17,6 +22,9 @@ templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
+    book = BookModel(keyword="파이썬", publisher="BJPublic", price=1200, image="me.png")
+    # save라는 함수가 async 함수 즉 코루틴 함수이기 때문에 await을 붙여준다.
+    await mongodb.engine.save(book)  # DB에 저장됨
     return templates.TemplateResponse(
         "./index.html", {"request": request, "title": "Book Collector"}
     )
@@ -27,3 +35,13 @@ async def search(request: Request, q: str):
     return templates.TemplateResponse(
         "./index.html", {"request": request, "title": "Book Collector", "keyword": q}
     )
+
+
+@app.on_event("startup")
+def on_app_start():
+    mongodb.connect()
+
+
+@app.on_event("shutdown")
+def on_app_shutdown():
+    mongodb.close()
