416ae8a96eb0e7c9d460909ceea10703afdfa477
Youngjin Jo <yjinjo@berkeley.edu>
2022-11-07 12:29:06 +0900

    #7. Deploy to AWS

--- server.py
+++ server.py
@@ -3,4 +3,6 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="localhost", port=8000, reload=True)
+    # 모든 사용자가 접속할 수 있도록 host는 0.0.0.0으로, port는 80으로 바꾼다.
+    # 80번 포트가 웹에서 기본적으로 작동하는 인터넷 port이다.
+    uvicorn.run("app.main:app", host="0.0.0.0", port=80, reload=False)
